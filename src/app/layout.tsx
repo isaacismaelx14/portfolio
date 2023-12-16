93684ddf5df080113ca14677b310489dd62483d2
Isaac Martinez <isaac@minnekdigital.com>
2023-12-15 21:13:42 -0400

    ci: update release-it configuration and dependencies, add og image
    - Modified .release-it.json to use the 'angular' preset for conventional changelog and streamlined the bumper configuration.
    - Downgraded release-it related packages in package.json for compatibility.
    - Added a new og.png image in the public directory for Open Graph metadata.
    - Enhanced the metadata in src/app/layout.tsx with detailed information and Open Graph (og) and Twitter card settings.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,28 @@ import { Providers } from './providers';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata = {
-    title: 'Isaac Martinez | Portfolio',
-    description: "Isaac's Portfolio",
+    title: 'Isaac Martinez - Full Stack Engineer | Portfolio',
+    description:
+        'Discover the innovative projects and professional journey of Isaac Martinez, a seasoned Full Stack Engineer with a passion for technology and creative problem-solving.',
     robots: 'index, follow',
     viewport: 'width=device-width, initial-scale=1',
     favicon: '/favicon.ico',
+    og: {
+        title: 'Isaac Martinez - Full Stack Engineer | Portfolio',
+        type: 'website',
+        url: 'https://www.isaacmartinez.dev/',
+        image: 'https://www.isaacmartinez.dev/og.png',
+        description:
+            'Step into the world of Isaac Martinez, where technology meets innovation. Explore my portfolio to see how I solve complex challenges with cutting-edge solutions.',
+    },
+    twitter: {
+        card: 'summary_large_image',
+        site: '@ismardev',
+        title: 'Isaac Martinez - Full Stack Engineer | Portfolio',
+        description:
+            "Embark on a journey through Isaac Martinez's professional landscape, featuring a robust portfolio of technological solutions and creative developments.",
+        image: 'https://www.isaacmartinez.dev/og.png',
+    },
 };
 
 export default function RootLayout({
